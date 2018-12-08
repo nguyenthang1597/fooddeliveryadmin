45eefbf3b30f7665d5d6807a6aac92d5412dabfa
Nguyen Thang <nguyenthang1597@gmail.com>
2018-12-08 15:19:11 +0700

    fix list res

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -10,7 +10,7 @@ router.get('/list', async (req, res) => {
   let perpage = req.query.perpage || 10;
   let list = await getAll(page, perpage);
   let total = (await countNumberRestaurant()).rows[0].total;
-  res.json({Total: total, page: page, pages: Math.round(total/perpage), perpage:perpage, Restaurants: list.rows });
+  res.json({Total: total, page: page, pages: total%perpage>0 ? total/perpage + 1 : total/perpage, perpage:perpage, Restaurants: list.rows });
 })
 
 function checkValidateResData(payload) {
