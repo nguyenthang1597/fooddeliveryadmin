cc64a42c8e5251fbb3db3eb8be2ae5e57329d526
Nguyen Thang <nguyenthang1597@gmail.com>
2018-12-08 23:13:24 +0700

    update get detail order

--- app/models/Order.js
+++ app/models/Order.js
@@ -1,6 +1,6 @@
 const {query} = require('./db');
 module.exports = {
   getAll: () => query('SELECT * FROM "Order" ORDER BY "BookAt" DESC'),
-  getDetail: id => query(`SELECT "Food"."Name", "OrderDetail"."Quatity", "Food"."Price", "Food"."PhotoUrl", "OrderDetail"."Note", "OrderDetail"."Quality" * "Food"."Price" as "Total" FROM "OrderDetail", "Food" WHERE "OrderDetail"."Id" = ${id} and "OrderDetail"."Food" = "Food"."Id"`),
+  getDetail: id => query(`SELECT "Food"."Name", "OrderDetail"."Quatity", "Food"."Price", "Food"."PhotoUrl", "OrderDetail"."Note", "OrderDetail"."Quatity" * "Food"."Price" as "Total" FROM "OrderDetail", "Food" WHERE "OrderDetail"."Id" = ${id} and "OrderDetail"."Food" = "Food"."Id"`),
   addNew: (Name, District, Ward, Street, Number) => query(`INSERT INTO "Order" ("Name","District", "Ward","Street", "Number") VALUES ('${Name}','${District}','${Ward}','${Street}','${Number}')`)
 }
