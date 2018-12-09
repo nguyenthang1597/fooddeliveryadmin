af9266bfce3c1ad6320dbd9d93cf2963ce6cbb96
Nguyen Thang <nguyenthang1597@gmail.com>
2018-12-09 10:13:09 +0700

    add count by category

--- app/models/Category.js
+++ app/models/Category.js
@@ -1,5 +1,6 @@
 const { query } = require("./db");
 
 module.exports = {
-  getAll: () => query('SELECT * FROM "Category"')
+  getAll: () => query('SELECT * FROM "Category"'),
+  countByCategory: () => query(`SELECT "Food"."Category" as "Id", "Category"."Name", Count("Food"."Id") as "Total" From "Category", "Food" Where "Category"."Id" = "Food"."Category" Group by "Food"."Category", "Category"."Name"`)
 }
--- routes/category.js
+++ routes/category.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
-const {getAll} = require('../app/models/Category')
+const {getAll, countByCategory} = require('../app/models/Category')
 
 
 router.get('/list',async (req, res) => {
   let result = await getAll();
+  return res.send(result.rows)
+})
+
+router.get('/countbycategory', async (req, res) => {
+  let result = await countByCategory();
   return res.send(result.rows)
 })
 
