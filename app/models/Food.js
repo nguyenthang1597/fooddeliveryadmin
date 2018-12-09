1f0aa4f8f778388f3d8e497a90a9e17b03f7a273
Nguyen Thang <nguyenthang1597@gmail.com>
2018-12-09 10:38:07 +0700

    add router food

--- app/models/Food.js
+++ app/models/Food.js
@@ -3,5 +3,6 @@ const { query }  = require('./db');
 module.exports = {
   createFood: (name, photoUrl, price) => {
     return query(`insert into "Food" ("Name", "PhotoUrl", "Price") values ('${name}', '${photoUrl}', ${price}) returning "Id"`)
-  }
+  },
+  count: () => query(`SELECT count("Id") as "Count" from "Food"`)
 }
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const restaurant = require('./restaurant')
 const address = require('./address');
 const order= require('./order')
 const category = require('./category')
+const food = require('./food')
 module.exports = (app) => {
     app.use('/auth', auth)
     app.use('/restaurant', restaurant)
     app.use('/address', address);
     app.use('/order', order);
     app.use('/category', category)
+    app.use('/food', food)
 }
