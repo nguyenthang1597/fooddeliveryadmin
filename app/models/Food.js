0731548435e3f8a7ba8f4962200f4475b7eaa6c3
Nguyen Thang <nguyenthang1597@gmail.com>
2018-12-16 20:50:55 +0700

    add update food

--- app/models/Food.js
+++ app/models/Food.js
@@ -4,5 +4,8 @@ module.exports = {
   createFood: (name, photoUrl, price) => {
     return query(`insert into "Food" ("Name", "PhotoUrl", "Price") values ('${name}', '${photoUrl}', ${price}) returning "Id"`)
   },
-  count: () => query(`SELECT count("Id") as "Count" from "Food"`)
+  count: () => query(`SELECT count("Id") as "Count" from "Food"`),
+  updateFood: (id, photoUrl, price, name) => {
+    return query(`update "Food" set "PhotoUrl" = '${photoUrl}', "Name" = '${name}', "Price" = '${price}' where "Id" = ${id}`)
+  }
 }
--- routes/food.js
+++ routes/food.js
@@ -1,9 +1,26 @@
 const router = require("express").Router();
-const {count} = require('../app/models/Food')
+const {count, updateFood} = require('../app/models/Food')
 
 router.get('/count', async (req, res) => {
   let result = await count();
   res.send({count: result.rows[0].Count})
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+      let result = await updateFood(req.params.id, req.body.PhotoUrl, req.body.Price, req.body.Name);
+      res.json({
+        Success: true
+      })
+  } catch (e) {
+    res.status(400).json({
+      Success: false
+    })
+  } finally {
+
+  }
+
+
 })
 
 
